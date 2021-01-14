89c1dd856a894dfb79f97500c214c461a13a33b8
almogre02 <almogre02@gmail.com>
2021-01-14 19:29:41 +0200

    include plane problem and Duel3d

---
+++ Codes/Duel3d/Player.java
@@ -0,0 +1,11 @@
+package Duel3d;
+
+public class Player {
+    String name;
+    boolean isAlive = true;
+    double p;
+    public Player(String name, double p) {
+        this.name = name;
+        this.p = p;
+    }
+}
